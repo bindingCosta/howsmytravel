36d1f9cc3b3b596a02e72a125cd0596a07df25a5
marcelo.costa <costanetomj@gmail.com>
2024-12-09 17:47:25 -0300

    Initial Commit

---
+++ howsmytravel/howsmytravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  howsmytravelApp.swift
+//  howsmytravel
+//
+//  Created by Marcelo Costa on 09/12/24.
+//
+
+import SwiftUI
+
+@main
+struct howsmytravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
